bc7ab2f16de332ec2812732bfc5f7bdf105255e0
Aliaksei Blizniuk <58771284+AlekseyBliznyuk@users.noreply.github.com>
2022-01-10 17:50:09 +0300

    Update 29.php

--- ООП/29.php
+++ ООП/29.php
@@ -1,9 +1,11 @@
 /* ООП. Static и final
 Задание: Напишите класс Singer, в котором объявите статическое свойство "name" равное "John", и статический метод toSing(), из которого нужно получить доступ к свойству "name" и вернуть его (используя return)
 */
+<?php
 class Singer {
     static $name = "John";
     static function toSing() {
         return Singer::$name;
     }
 }
+?>
