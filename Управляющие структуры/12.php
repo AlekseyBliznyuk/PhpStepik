3e4af42d0399f0cbddd30f7c34954cb4bf852c15
Aliaksei Blizniuk <58771284+AlekseyBliznyuk@users.noreply.github.com>
2022-01-10 17:51:00 +0300

    Update 12.php

--- Управляющие структуры/12.php
+++ Управляющие структуры/12.php
@@ -1,7 +1,9 @@
 /* Управляющие структуры. Цикл foreach
 Задание: вывести элементы массива с помощью цикла foreach
 */
+<?
 $items = array("one", "two", "three");
 foreach ($items as $item) {
 echo "$item\n";
 }
+?>
